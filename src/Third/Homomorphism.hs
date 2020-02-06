9e5ab0d6bc102b9741ae75d014a8126e5d80e9f6
John Ky <newhoggy@gmail.com>
2020-02-06 14:30:48 +1100

    mappendTree

--- src/Third/Homomorphism.hs
+++ src/Third/Homomorphism.hs
@@ -2,6 +2,7 @@ module Third.Homomorphism
   ( Tree(..)
   , Zipper
   , z2t
+  , mappendTree
   ) where
 
 data Tree a = Bin a (Tree a) (Tree a) | Tip
@@ -13,3 +14,7 @@ z2t :: Zipper a -> Tree a
 z2t []                 = Tip
 z2t (Left  (a, lt):rz) = Bin a  lt       (z2t rz)
 z2t (Right (a, rt):lz) = Bin a (z2t lz)   rt
+
+mappendTree :: Monoid a => Tree a -> a
+mappendTree Tip           = mempty
+mappendTree (Bin a lt rt) = a <> mappendTree lt <> mappendTree rt
