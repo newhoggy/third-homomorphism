958e51af9532442d12f4433045c771c0b9c12e57
John Ky <newhoggy@gmail.com>
2020-02-06 14:28:39 +1100

    Tree and Zipper

--- src/Third/Homomorphism.hs
+++ src/Third/Homomorphism.hs
@@ -1,3 +1,15 @@
 module Third.Homomorphism
-  (
+  ( Tree(..)
+  , Zipper
+  , z2t
   ) where
+
+data Tree a = Bin a (Tree a) (Tree a) | Tip
+  deriving (Eq, Show)
+
+type Zipper a = [Either (a, Tree a) (a, Tree a)]
+
+z2t :: Zipper a -> Tree a
+z2t []                 = Tip
+z2t (Left  (a, lt):rz) = Bin a  lt       (z2t rz)
+z2t (Right (a, rt):lz) = Bin a (z2t lz)   rt
