f4a60ce8e06ae6ea101cef37da86a3e45b1fed41
John Ky <newhoggy@gmail.com>
2020-02-06 18:11:09 +1100

    Traced maxPath

--- src/Third/Homomorphism.hs
+++ src/Third/Homomorphism.hs
@@ -31,15 +31,18 @@ maxPathRi (m, w) = [Left (w, Bin (m - w) Tip Tip)]
 maxPathRiAppend :: (Int, Int) -> (Int, Int) -> (Int, Int)
 maxPathRiAppend (m1, w1) (m2, w2) = maxPathDn (maxPathRi (m1, w1) ++ maxPathRi (m2, w2))
 
-maxPathPar :: Zipper Int -> Int
-maxPathPar = fst . go
-  where go :: Zipper Int -> (Int, Int)
-        go []             = (0, 0)
-        go [Left  (n, t)] = (n + maxPathSimple t, n)
-        go [Right (n, t)] = (n + maxPathSimple t, n)
-        go zs = (max m1 (w1 + m2), w1 + w2)
-          where j         = length zs `div` 2
-                zl        = take j zs
-                zr        = drop j zs
-                (m1, w1)  = go zl
-                (m2, w2)  = go zr
+maxPath :: (Tree Int -> Zipper Int) -> Tree Int -> (Word, Int)
+maxPath f t = maxPathPar f (f t)
+
+maxPathPar :: (Tree Int -> Zipper Int) ->  Zipper Int -> (Word, Int)
+maxPathPar f z = fmap fst (go z)
+  where go :: Zipper Int -> (Word, (Int, Int))
+        go []             = (0, (0, 0))
+        go [Left  (n, t)] = let (q, x) = maxPath f t in (q, (n + x, n))
+        go [Right (n, t)] = let (q, x) = maxPath f t in (q, (n + x, n))
+        go zs = (max q1 q2 + 1, (max m1 (w1 + m2), w1 + w2))
+          where j               = length zs `div` 2
+                zl              = take j zs
+                zr              = drop j zs
+                (q1, (m1, w1))  = go zl
+                (q2, (m2, w2))  = go zr
